0a3faf111f295be82f0e5651dd5694bc798f9c24
foyzan <foyzanahmedapon01@gmail.com>
2026-04-13 12:17:31 +0600

    learing playground setup

---
+++ src/index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+const http = require('http');
+const app = require('./app');
+const connectDB = require('./db/connectDB');
+const server = http.createServer(app)
+
+const  main = async () =>{
+    await connectDB();
+    const port = process.env.PORT || 4000
+    server.listen(port, async ()=>{
+        console.log('server is running on port ' + port)
+    })
+}
+
+main().catch((err)=>{console.log(err)})
